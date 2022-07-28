b30f1f7ee38932d0f79c2f69e8c219d21b8ad451
yingying.xwy <yingying.xwy@antgroup.com>
2022-07-28 11:13:22 +0800

    feat: add the link action for the banner buttons

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { history } from 'umi';
+import { Anchor } from 'antd';
 import { Space, Button } from 'antd';
 import { Header } from '../Header';
 
 import styles from './index.less';
+
+const { Link } = Anchor;
+
+export interface BannerButtonProps {
+  text: string;
+  url: string;
+}
 
 export const Banner = ({
   slogan,
@@ -16,7 +25,7 @@ export const Banner = ({
   bgUrl: string;
   subTitle?: string;
   activeKey?: string;
-  buttons?: string[];
+  buttons?: BannerButtonProps[];
   notice?: string;
 }) => {
   return (
@@ -26,18 +35,27 @@ export const Banner = ({
       <div className={styles.slogan}>{slogan}</div>
       {subTitle && <div className={styles.subTitle}>{subTitle}</div>}
 
-      <Space className={styles.btnGroup} size={30}>
-        {buttons?.[0] && (
-          <Button type="primary" block className="lightBtn">
-            {buttons?.[0]}
-          </Button>
-        )}
-        {buttons?.[1] && (
-          <Button type="primary" block className="darkBtn">
-            {buttons?.[1]}
-          </Button>
-        )}
-      </Space>
+      {buttons?.length > 0 && (
+        <Space className={styles.btnGroup} size={30}>
+          {buttons?.[0] && (
+            <Anchor affix={false} className={styles.btn}>
+              <Link href={buttons[0].url} title={buttons[0].text} />
+            </Anchor>
+          )}
+          {buttons?.[1] && (
+            <Button
+              type="primary"
+              block
+              className="darkBtn"
+              onClick={() => {
+                history.push(buttons[1].url);
+              }}
+            >
+              {buttons[1].text}
+            </Button>
+          )}
+        </Space>
+      )}
     </div>
   );
 };
