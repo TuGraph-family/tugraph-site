da2576c16ab3401e074db5f25b470847ab7e858c
yingying.xwy <yingying.xwy@antgroup.com>
2022-07-29 16:27:55 +0800

    feat: tweak the style of the banner for the mobile view

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,6 +3,8 @@ import { history } from 'umi';
 import { Anchor } from 'antd';
 import { Space, Button } from 'antd';
 import { Header } from '../Header';
+import { useMedia } from 'react-use';
+import cx from 'classnames';
 
 import styles from './index.less';
 
@@ -28,34 +30,44 @@ export const Banner = ({
   buttons?: BannerButtonProps[];
   notice?: string;
 }) => {
+  const isWide = useMedia('(min-width: 767.99px)', true);
   return (
     <div className={styles.banner} style={{ backgroundImage: `url(${bgUrl})` }}>
       <Header activeKey={activeKey} />
-      {notice && <div className={styles.notice}>{notice}</div>}
-      <div className={styles.slogan}>{slogan}</div>
-      {subTitle && <div className={styles.subTitle}>{subTitle}</div>}
+      <div className={styles.containerWrapper}>
+        {notice && <div className={styles.notice}>{notice}</div>}
+        <div className={styles.slogan}>{slogan}</div>
+        {subTitle && <div className={styles.subTitle}>{subTitle}</div>}
 
-      {buttons?.length > 0 && (
-        <Space className={styles.btnGroup} size={30}>
-          {buttons?.[0] && (
-            <Anchor affix={false} className={styles.btn}>
-              <Link href={buttons[0].url} title={buttons[0].text} />
-            </Anchor>
-          )}
-          {buttons?.[1] && (
-            <Button
-              type="primary"
-              block
-              className="darkBtn"
-              onClick={() => {
-                history.push(buttons[1].url);
-              }}
-            >
-              {buttons[1].text}
-            </Button>
-          )}
-        </Space>
-      )}
+        {buttons?.length > 0 && (
+          <Space className={styles.btnGroup} size={isWide ? 30 : 0}>
+            {buttons?.[0] && (
+              <Anchor
+                affix={false}
+                className={cx(
+                  styles.primaryBtn,
+                  isWide ? null : styles.primaryBtnMobile,
+                )}
+              >
+                <Link href={buttons[0].url} title={buttons[0].text} />
+              </Anchor>
+            )}
+            {buttons?.[1] && (
+              <Button
+                type="primary"
+                block
+                ghost={!isWide}
+                className={isWide ? 'darkBtn' : styles.darkBtnMobile}
+                onClick={() => {
+                  history.push(buttons[1].url);
+                }}
+              >
+                {buttons[1].text}
+              </Button>
+            )}
+          </Space>
+        )}
+      </div>
     </div>
   );
 };
