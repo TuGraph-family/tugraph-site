65a4c5158e0134243d52f8ad5cccae6ade603374
yuaiai0419 <yay01169903@antgroup.com>
2023-02-14 17:38:40 +0800

    fix: 移动端产品中英文样式修改

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -92,14 +92,14 @@ export default function ProductPage() {
               width: isWide ? 362 : '100%',
             }}
           />
-          <div className={styles.videoInfo}>
+          <Space className={styles.videoInfo} align="start">
             <div className={styles.title}>
               {intl.formatMessage({ id: 'product.video0' })}
             </div>
             <Tag className={styles.tag}>
               {intl.formatMessage({ id: 'product.videoTime0' })}
             </Tag>
-          </div>
+          </Space>
         </div>
         <div className={styles.card}>
           <JoLPlayer
@@ -110,14 +110,14 @@ export default function ProductPage() {
               width: isWide ? 362 : '100%',
             }}
           />
-          <div className={styles.videoInfo}>
+          <Space className={styles.videoInfo} align="start">
             <div className={styles.title}>
               {intl.formatMessage({ id: 'product.video1' })}
             </div>
             <Tag className={styles.tag}>
               {intl.formatMessage({ id: 'product.videoTime1' })}
             </Tag>
-          </div>
+          </Space>
         </div>
         <div className={styles.card}>
           <JoLPlayer
@@ -129,14 +129,14 @@ export default function ProductPage() {
             }}
           />
 
-          <div className={styles.videoInfo}>
+          <Space className={styles.videoInfo} align="start">
             <div className={styles.title}>
               {intl.formatMessage({ id: 'product.video2' })}
             </div>
             <Tag className={styles.tag}>
               {intl.formatMessage({ id: 'product.videoTime2' })}
             </Tag>
-          </div>
+          </Space>
         </div>
       </Carousel>
     </div>
@@ -163,7 +163,10 @@ export default function ProductPage() {
         <div>{intl.formatMessage({ id: 'product.introduction1' })}</div>
       </div>
 
-      <SubTitle title={intl.formatMessage({ id: 'product.feature' })} />
+      <SubTitle
+        title={intl.formatMessage({ id: 'product.feature' })}
+        className={styles.subTitle}
+      />
       <div className="maxContainer">
         <Row className={styles.featCards}>
           {getFeats(intl)?.map((item, key) => {
