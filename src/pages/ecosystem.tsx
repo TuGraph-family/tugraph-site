f82ddf8809e62462c530dca986434a88130607b8
yuaiai0419 <yay01169903@antgroup.com>
2023-02-14 18:39:18 +0800

    fix: 移动生态例中英文样式修改

--- src/pages/ecosystem.tsx
+++ src/pages/ecosystem.tsx
@@ -39,12 +39,7 @@ export default function EcosystemPage() {
                     <img src={item.src} alt="icon" />
                     <div>
                       <div className={styles.title}>{item.title}</div>
-                      <div
-                        className={styles.desc}
-                        style={{ color: 'rgba(26, 27, 37, 0.45)' }}
-                      >
-                        {item.desc}
-                      </div>
+                      <div className={styles.description}>{item.desc}</div>
                     </div>
                   </div>
                 </Col>
@@ -68,10 +63,14 @@ export default function EcosystemPage() {
                   <div
                     className={cx(styles.supportItem, {
                       [styles.enSupportItemPadding]:
-                        index === 0 && lang === 'en',
+                        index === 0 && (lang === 'en' || lang === 'en-US'),
                     })}
                   >
-                    <Space size={12} align="center" style={{ height: '100%' }}>
+                    <Space
+                      size={12}
+                      align={isWide ? 'center' : 'start'}
+                      style={{ height: '100%' }}
+                    >
                       <img src={item.src} alt="icon" />
                       <div>{item.support}</div>
                     </Space>
