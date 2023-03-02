12dfe38ab543df06c1dd29ddac8c1cbacd3ca48e
SputnikQ <88693478+SputnikQ@users.noreply.github.com>
2023-03-02 20:45:26 +0800

    feat: add search component supported by algolia (#59)
    * fix: 修复二级页面导航栏丢失
    
    * feat: add search component supported by algolia
    
    * feat: add search component supported by algolia
    
    * feat: add google ads script
    
    * style: searchBtn style optimization
    
    * style: Adjust the search style
    
    * style: change search length
    
    ---------
    
    Co-authored-by: qmy01212463 <qmy01212463@antgroup.com>

--- src/constant.ts
+++ src/constant.ts
@@ -2,3 +2,13 @@ export const DOC_HOST_ZH = 'https://webapi.oceanbase.com';
 export const DOC_HOST_EN = 'https://wan-api.oceanbase.com';
 export const HOST_ZH = 'https://tugraph.antgroup.com';
 export const HOST_EN = 'https://www.tugraph.org';
+export const searchParamsEn = {
+  appId: 'EZGAVAOMAN',
+  indexName: 'tugraph',
+  apiKey: 'f4bdd9b83f71b2c95980930921907572',
+};
+export const searchParamsZh = {
+  appId: 'NDSB2X7NZ3',
+  indexName: 'tugraph-antgroup',
+  apiKey: '543db0cadd3173a4b801cd13f89aa996',
+};
