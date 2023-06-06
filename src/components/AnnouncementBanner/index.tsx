89f214d98d674795fdcd33c9cf638ee45e302b81
qmy01212463 <qmy01212463@antgroup.com>
2023-06-06 10:40:56 +0800

    chore: Ad removed

--- src/components/AnnouncementBanner/index.tsx
+++ src/components/AnnouncementBanner/index.tsx
@@ -1,17 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Carousel, Drawer, Modal, Tooltip } from 'antd';
-import { getLocale, history } from 'umi';
-import {
-  LeftOutlined,
-  RightOutlined,
-  CloseOutlined,
-  DoubleLeftOutlined,
-  ArrowRightOutlined,
-} from '@ant-design/icons';
-import { useMedia } from 'react-use';
-import { useIntl } from 'umi';
-import styles from './index.less';
 import { getBannerContentList } from '@/data/bannerContent';
+import { CloseOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { Carousel, Drawer, Modal, Tooltip } from 'antd';
+import { useEffect, useState } from 'react';
+import { useMedia } from 'react-use';
+import { getLocale, history, useIntl } from 'umi';
+import styles from './index.less';
 const AnnouncementBanner = () => {
   const [showBottomDrawer, setShowBottomDrawer] = useState(false);
   const [showBottomModal, setShowBottomModal] = useState(true);
@@ -30,107 +23,111 @@ const AnnouncementBanner = () => {
   const onOpenBanner = () => {
     setShowBottomModal(true);
   };
-  const pcBanner = lang === 'zh-CN' && (
-    <>
-      <Tooltip
-        title={intl.formatMessage({ id: 'home.banner.expandBtnDesc' })}
-        color="#FFFFFF"
-        placement="left"
-      >
-        <div
-          className={styles.expandBtn}
-          onClick={() => {
-            setShowBottomDrawer(true);
-          }}
+  const pcBanner = lang === 'zh-CN' &&
+    getBannerContentList(intl).length > 0 && (
+      <>
+        <Tooltip
+          title={intl.formatMessage({ id: 'home.banner.expandBtnDesc' })}
+          color="#FFFFFF"
+          placement="left"
         >
-          <img
-            src="https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*WjEsS552EJAAAAAAAAAAAAAADgOBAQ/original"
-            alt=""
-          />
-        </div>
-      </Tooltip>
-      <Drawer
-        placement="right"
-        open={showBottomDrawer}
-        mask={false}
-        height="192"
-        closable={false}
-        className={styles.bottomDrawer}
-      >
-        <div className={styles.closeOutlinedBtn}>
-          <CloseOutlined
+          <div
+            className={styles.expandBtn}
             onClick={() => {
-              setShowBottomDrawer(false);
+              setShowBottomDrawer(true);
+            }}
+          >
+            <img
+              src="https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*WjEsS552EJAAAAAAAAAAAAAADgOBAQ/original"
+              alt=""
+            />
+          </div>
+        </Tooltip>
+
+        <Drawer
+          placement="right"
+          open={showBottomDrawer}
+          mask={false}
+          height="192"
+          closable={false}
+          className={styles.bottomDrawer}
+        >
+          <div className={styles.closeOutlinedBtn}>
+            <CloseOutlined
+              onClick={() => {
+                setShowBottomDrawer(false);
+                sessionStorage.setItem('isBannerShow', 'false');
+              }}
+            />
+          </div>
+          {getBannerContentList(intl).length > 1 && (
+            <>
+              <div className={styles.leftOutlinedBtn}>
+                <LeftOutlined />
+              </div>
+              <div className={styles.rightOutlinedBtn}>
+                <RightOutlined />
+              </div>
+            </>
+          )}
+          <Carousel>
+            {getBannerContentList(intl).map((item, index) => (
+              <div key={index} className={styles.bannerContainer}>
+                <img
+                  src={item.pcImg}
+                  onClick={() => {
+                    setShowBottomDrawer(false);
+                    sessionStorage.setItem('isBannerShow', 'false');
+                    history.push('/blog?id=14');
+                  }}
+                />
+              </div>
+            ))}
+          </Carousel>
+        </Drawer>
+      </>
+    );
+  const mobileBanner = lang === 'zh-CN' &&
+    getBannerContentList(intl).length > 0 && (
+      <>
+        <img
+          src="https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*E6rZRLlvvDoAAAAAAAAAAAAADgOBAQ/original"
+          className={styles.announcementIcon}
+          onClick={() => {
+            onOpenBanner();
+          }}
+        />
+
+        <Modal
+          className={styles.bannerModal}
+          open={showBottomModal}
+          closable={false}
+          footer={null}
+        >
+          <Carousel>
+            {getBannerContentList(intl).map((item, index) => (
+              <div key={index} className={styles.mobileContainer}>
+                <img
+                  src={item.mobileImg}
+                  onClick={() => {
+                    history.push('/blog?id=14');
+                    setShowBottomModal(false);
+                    sessionStorage.setItem('isBannerShow', 'false');
+                  }}
+                />
+              </div>
+            ))}
+          </Carousel>
+          <CloseOutlined
+            className={styles.mobileCloseIcon}
+            onClick={() => {
+              setShowBottomModal(false);
               sessionStorage.setItem('isBannerShow', 'false');
             }}
           />
-        </div>
-        {getBannerContentList(intl).length > 1 && (
-          <>
-            <div className={styles.leftOutlinedBtn}>
-              <LeftOutlined />
-            </div>
-            <div className={styles.rightOutlinedBtn}>
-              <RightOutlined />
-            </div>
-          </>
-        )}
-        <Carousel>
-          {getBannerContentList(intl).map((item, index) => (
-            <div key={index} className={styles.bannerContainer}>
-              <img
-                src={item.pcImg}
-                onClick={() => {
-                  setShowBottomDrawer(false);
-                  sessionStorage.setItem('isBannerShow', 'false');
-                  history.push('/blog?id=14');
-                }}
-              />
-            </div>
-          ))}
-        </Carousel>
-      </Drawer>
-    </>
-  );
-  const mobileBanner = lang === 'zh-CN' && (
-    <>
-      <img
-        src="https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*E6rZRLlvvDoAAAAAAAAAAAAADgOBAQ/original"
-        className={styles.announcementIcon}
-        onClick={() => {
-          onOpenBanner();
-        }}
-      />
-      <Modal
-        className={styles.bannerModal}
-        open={showBottomModal}
-        closable={false}
-        footer={null}
-      >
-        <Carousel>
-          {getBannerContentList(intl).map((item, index) => (
-            <div key={index} className={styles.mobileContainer}>
-              <img
-                src={item.mobileImg}
-                onClick={() => {
-                  history.push('/blog?id=14');
-                  setShowBottomModal(false);
-                  sessionStorage.setItem('isBannerShow', 'false');
-                }}
-              />
-            </div>
-          ))}
-        </Carousel>
-        <CloseOutlined
-          className={styles.mobileCloseIcon}
-          onClick={() => {
-            setShowBottomModal(false);
-            sessionStorage.setItem('isBannerShow', 'false');
-          }}
-        />
-      </Modal>
-    </>
-  );
+        </Modal>
+      </>
+    );
   return isWide ? pcBanner : mobileBanner;
 };
 export default AnnouncementBanner;
--- src/data/bannerContent.ts
+++ src/data/bannerContent.ts
@@ -1,9 +1,2 @@
 import { IntlShape } from 'react-intl';
-export const getBannerContentList = (intl: IntlShape) => [
-  {
-    pcImg:
-      'https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*lHujTrMtso0AAAAAAAAAAAAADgOBAQ/original',
-    mobileImg:
-      'https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*xMiIQ6u-aLIAAAAAAAAAAAAADgOBAQ/original',
-  },
-];
+export const getBannerContentList = (intl: IntlShape) => [];
