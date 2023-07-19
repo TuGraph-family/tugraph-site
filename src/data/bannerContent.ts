ddd9dd31c07a6c8059007b44754bb249a8f4b302
yingying <yingying.xwy@antgroup.com>
2023-07-19 21:36:28 +0800

    style: fix blog

--- src/data/bannerContent.ts
+++ src/data/bannerContent.ts
@@ -2,9 +2,9 @@ import { IntlShape } from 'react-intl';
 export const getBannerCnContentList = (intl: IntlShape) => [
   {
     pcImg:
-      'https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*eKGCQo3ndGwAAAAAAAAAAAAADgOBAQ/original',
+      'https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*PHPeRqtm0iAAAAAAAAAAAAAADgOBAQ/original',
     mobileImg:
-      'https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*vJT-RavS3vkAAAAAAAAAAAAADgOBAQ/original',
+      'https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*1uAORLFAksIAAAAAAAAAAAAADgOBAQ/original',
   },
 ];
 export const getBannerEnContentList = (intl: IntlShape) => [
