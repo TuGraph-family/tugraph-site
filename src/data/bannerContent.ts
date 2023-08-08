77c24f7f3977c84ccdf20a6893c98241cc0e084e
Moyee <576375879@qq.com>
2023-08-08 04:38:28 -0700

    fix: update banner picture (#87)

--- src/data/bannerContent.ts
+++ src/data/bannerContent.ts
@@ -2,16 +2,16 @@ import { IntlShape } from 'react-intl';
 export const getBannerCnContentList = (intl: IntlShape) => [
   {
     pcImg:
-      'https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*PHPeRqtm0iAAAAAAAAAAAAAADgOBAQ/original',
+      'https://mdn.alipayobjects.com/huamei_2dxjn8/afts/img/A*Pp1rRbw00kIAAAAAAAAAAAAADrR-AQ/original',
     mobileImg:
-      'https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*1uAORLFAksIAAAAAAAAAAAAADgOBAQ/original',
+      'https://mdn.alipayobjects.com/huamei_2dxjn8/afts/img/A*QY_7S4hG8T8AAAAAAAAAAAAADrR-AQ/original',
   },
 ];
 export const getBannerEnContentList = (intl: IntlShape) => [
   {
     pcImg:
-      'https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*wzGLRIiVREEAAAAAAAAAAAAADgOBAQ/original',
+      'https://mdn.alipayobjects.com/huamei_2dxjn8/afts/img/A*MhtgQJHk8soAAAAAAAAAAAAADrR-AQ/original',
     mobileImg:
-      'https://mdn.alipayobjects.com/huamei_qcdryc/afts/img/A*VZg-R7YLE68AAAAAAAAAAAAADgOBAQ/original',
+      'https://mdn.alipayobjects.com/huamei_2dxjn8/afts/img/A*mb5dTZkyyG8AAAAAAAAAAAAADrR-AQ/original',
   },
 ];
