7c0ca71195a998248c27a206208c27bbbd66c8e5
qiming <138841683+noneAuth@users.noreply.github.com>
2024-03-19 15:31:33 +0800

    Update lang (#136)
    * feat: lang

--- src/constant.ts
+++ src/constant.ts
@@ -1,7 +1,9 @@
 export const DOC_HOST_ZH = 'https://webapi.oceanbase.com';
 export const DOC_HOST_EN = 'https://wan-api.oceanbase.com';
-export const HOST_ZH = 'https://tugraph.antgroup.com';
-export const HOST_EN = 'https://www.tugraph.org';
+export const HOST_ZH = 'https://tugraph.tech?lang=zh-CN';
+export const HOST_EN = 'https://tugraph.tech?lang=en-US';
+export const ZH_SITE = 'https://tugraph.antgroup.com';
+export const EN_SITE = 'https://www.tugraph.org';
 export const searchParamsEn = {
   appId: 'EZGAVAOMAN',
   indexName: 'tugraph',
@@ -57,3 +59,5 @@ export const logoList = [
     height: 84,
   },
 ];
+export const DEFAULT_LOCAL = 'zh-CN';
+export const MATCH_LOCAL_SEARCH_REG = /[\\?]?([a-zA-Z0-9]+)=([0-9A-Z\-a-z]+)/g;
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,6 @@
+/* eslint-disable no-cond-assign */
+import { MATCH_LOCAL_SEARCH_REG } from '@/constant';
+
 /** 中英文 banner 标题的 marginTop */
 export const ChOrEnStyle = (
   isChinese: boolean,
@@ -5,6 +8,19 @@ export const ChOrEnStyle = (
   type: 'description' | 'slogan',
 ) => {
   if (type === 'slogan') return isHome ? '80px' : isChinese ? '160px' : '120px';
-  if (type === 'description')
+  if (type === 'description') {
     return isHome ? '16px' : isChinese ? '8px' : '16px';
+  }
+};
+
+/** 获取search参数  */
+
+export const getSearch = (search: string) => {
+  if (!search) return false;
+  const result: any = {};
+  let match: any = null;
+  while ((match = MATCH_LOCAL_SEARCH_REG.exec(search))) {
+    result[match[1]] = match[2];
+  }
+  return result;
 };
